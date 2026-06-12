dd1ce484314c944d5029ed948392e4511e0bfbb3
ziad-tariq <103075559+Ziad-Tariq@users.noreply.github.com>
2026-06-12 14:57:32 +0500

    add button to resize grid

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const container = document.querySelector("#container");
 
-for (i=0; i<256; i++) {
+function createGrid(size){
+
+for (let i=0; i<size*size; i++) {
     const divCreation=document.createElement("div")
     divCreation.classList.add("square");
 
       divCreation.addEventListener("mouseover", () => {
         divCreation.classList.add("permahover");
     });
+
     container.appendChild(divCreation)
-}
+    
+} }
 
-const button = document.querySelector("#buttonStyle");
-button.addEventListener("click", {
-
-});
+createGrid(16);
 
 function makeSize(){
 
-    let size=prompt("Enter the number of squares per side (maximum 100):")
+    let newSize=parseInt(prompt("Enter the number of squares per side (maximum 100):"))
+
+    if(newSize>100){
+        return alert("The Maximum Grid size is 100");  
+    }
+
+    return newSize;
 }
+
+
+const button = document.querySelector("#buttonStyle");
+button.addEventListener("click", () => {
+
+    container.innerHTML="";
+    createGrid(makeSize());
+    
+} );
